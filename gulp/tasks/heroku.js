b09077e447da8d150023e8bdf6877ae81853ba96
ngocsangyem <nnsang24@gmail.com>
2019-11-18 16:00:55 +0700

    add

--- gulp/tasks/heroku.js
+++ gulp/tasks/heroku.js
@@ -3,5 +3,7 @@ import gulp from 'gulp';
 import { plugins, args, config, taskTarget, browserSync } from '../utils';
 
 gulp.task('deploy:heroku', () => {
-	return gulp.src('./index.php').pipe(gulp.dest(`${taskTarget}`));
+	return gulp
+		.src(['./index.php', './composer.json'])
+		.pipe(gulp.dest(`${taskTarget}`));
 });
